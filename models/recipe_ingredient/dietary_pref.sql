1db736a6610434d320671adcd8f26698d209edce
Nguyen Hoang To Nhu <73817808+lacnham@users.noreply.github.com>
2023-04-26 13:34:40 +0700

    update table value

--- models/recipe_ingredient/dietary_pref.sql
+++ models/recipe_ingredient/dietary_pref.sql
@@ -3,10 +3,19 @@ CREATE TABLE IF NOT EXISTS dietary_pref (name varchar PRIMARY KEY);
 INSERT INTO
   dietary_pref (name)
 VALUES
+  ('Omnivorous'),
   ('Vegetarian'),
   ('Vegan'),
-  ('Keto'),
+  ('Pescatarian'),
+  ('Flexitarian'),
   ('Paleo'),
+  ('Keto'),
+  ('Mediterranean'),
   ('Gluten-free'),
-  ('Dairy-free')
+  ('Dairy-free'),
+  ('Low-carb'),
+  ('Low-fat'),
+  ('High-protein'),
+  ('Raw food'),
+  ('Fruitarian')
   ON CONFLICT DO NOTHING;
