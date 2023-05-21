9f643ce324eb78625b2004ce8dcf69302c6331da
Linh-0v0 <s3864120@rmit.edu.vn>
2023-05-21 21:23:38 +0700

    Update release.js

--- release.js
+++ release.js
@@ -1,4 +1,6 @@
 const { exec } = require('node:child_process')
+const path = require('path');
+const mainSqlPath = path.join(__dirname, 'models', 'main.sql');
 require('dotenv').config()
 
 try {
@@ -6,11 +8,10 @@ try {
   exec('node index.js')
 
   // Run main.sql to initialize other tables
-  // execSync('psql -U your_username -f models/main.sql')
-  // execSync(`PGPASSWORD=${dbPassword} psql $DATABASE_URL -f models/main.sql`)
-  // execSync(`psql ${databaseURL} -U your_username -h your_host -p your_port -d your_database_name -w -f models/main.sql`)
   // execSync(`psql ${process.env.DATABASE_URI} -f models/main.sql`)
-  exec(`psql -U ${process.env.DB_USER} -h ${process.env.DB_HOST} -p ${process.env.DB_PORT} -d ${process.env.DB_NAME} -w ${process.env.DB_PASSWORD} -f models/main.sql`)
+  exec(
+    `psql -U ${process.env.DB_USER} -h ${process.env.DB_HOST} -p ${process.env.DB_PORT} -d ${process.env.DB_NAME} -w ${process.env.DB_PASSWORD} -f ${mainSqlPath}`
+  )
 
   console.log('Release.js runs successfully.')
 } catch (error) {
